0087563e29071134ed5501d501604f4a285b3ccc
andez9854 <arjelson@go.olemiss.edu>
2020-11-16 14:28:50 -0600

    Pre-Presentation edits
    Added Documentation, Fixed Practice.py (classification NN), Added SciPy Delaunay Functionality

--- Algorithms.py
+++ Algorithms.py
@@ -50,27 +50,29 @@ mesh = (o3d.io.read_triangle_mesh("STLs/dragon.stl"))
 cloud = o3d.geometry.PointCloud()
 cloud.points = mesh.vertices
 tetra_mesh, pt_map = o3d.geometry.TetraMesh.create_from_point_cloud(cloud)
-
 for alpha in np.logspace(np.log10(5), np.log10(0.25), num=5):
     print(f"alpha={alpha:.3f}")
     Tmesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(cloud, alpha, tetra_mesh, pt_map)
     Tmesh.compute_vertex_normals()
     o3d.visualization.draw_geometries([Tmesh])
 
-#SciPy Delaunay Triangulation
-points = np.array(mesh.vertices)
+#SciPy Delaunay Triangulation - 30 min compile time. Creates a mesh inside a tetramesh
+points = np.asarray(mesh.vertices)
 tri = Delaunay(points) # points: np.array() of 3d points 
 indices = tri.simplices
 vertices = points[indices]
+print(tri)
+#o3d.visualization.draw_geometries([tri])
 
+#Code used to plot the Delaunay
 def plot_tri(ax, points, tri):
     edges = collect_edges(tri)
     x = np.array([])
     y = np.array([])
     z = np.array([])
     for (i,j) in edges:
-        x = np.append(x, [points[i, 0], points[j, 0], np.nan])      
-        y = np.append(y, [points[i, 1], points[j, 1], np.nan])      
+        x = np.append(x, [points[i, 0], points[j, 0], np.nan])
+        y = np.append(y, [points[i, 1], points[j, 1], np.nan])
         z = np.append(z, [points[i, 2], points[j, 2], np.nan])
     ax.plot3D(x, y, z, color='g', lw='0.1')
 
@@ -78,10 +80,9 @@ def plot_tri(ax, points, tri):
 
 def collect_edges(tri):
     edges = set()
-
     def sorted_tuple(a,b):
         return (a,b) if a < b else (b,a)
-    # Add edges of tetrahedron (sorted so we don't add an edge twice, even if it comes in reverse order).
+    #Add edges of tetrahedron (sorted so we don't add an edge twice, even if it comes in reverse order).
     for (i0, i1, i2, i3) in tri.simplices:
         edges.add(sorted_tuple(i0,i1))
         edges.add(sorted_tuple(i0,i2))
@@ -91,7 +92,7 @@ def collect_edges(tri):
         edges.add(sorted_tuple(i2,i3))
     return edges
 
-
 fig = mplot.figure()
 axes = mplot3d.Axes3D(fig)
 plot_tri(axes, points, tri)
+mplot.show()
--- Practice/Practice.py
+++ Practice/Practice.py
@@ -11,7 +11,6 @@ import h5py
 import pickle
 import open3d as o3d
 
-
 def array_to_color(array, cmap="Oranges"):
     s_m = plt.cm.ScalarMappable(cmap=cmap)
     return s_m.to_rgba(array)[:,:-1]
@@ -20,10 +19,9 @@ def rgb_data_transform(data):
     data_t = []
     for i in range(data.shape[0]):
         data_t.append(array_to_color(data[i]).reshape(16, 16, 16, 3))
-    #print(np.asarray(data_t).shape)
     return np.asarray(data_t, dtype=np.float32)
 
-with h5py.File("./full_dataset_vectors.h5", "r") as hf:
+with h5py.File("./Practice/full_dataset_vectors.h5", "r") as hf:
     xtrain = hf["X_train"][:]
     targets_train = hf["y_train"][:]
     xtest = hf["X_test"][:] 
@@ -34,10 +32,8 @@ with h5py.File("./full_dataset_vectors.h5", "r") as hf:
     xtest = rgb_data_transform(xtest)
     targets_train = to_categorical(targets_train).astype(np.integer)
     targets_test = to_categorical(targets_test).astype(np.integer)
-
-print(xtrain)
-
-
+"""
+#Building the model in Keras
 model = Sequential()
 model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=sample_shape))
 model.add(MaxPooling3D(pool_size=(2, 2, 2)))
@@ -52,48 +48,26 @@ model.add(Dense(256, activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(256, activation='relu', kernel_initializer='he_uniform'))
 model.add(Dense(10, activation='softmax'))
 
-model.compile(loss='categorical_crossentropy',
-              optimizer=keras.optimizers.Adam(lr=0.001),
-              metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(lr=0.001), metrics=['accuracy'])
 model.summary()
 
 # Fit data to model
-history = model.fit(xtrain, targets_train,
-            batch_size=128,
-            epochs=3,
-            verbose=1,
-            validation_split=0.3)
+history = model.fit(xtrain, targets_train, batch_size=128, epochs=1000, verbose=1, validation_split=0.3)
 
 score = model.evaluate(xtest,targets_test,verbose=1)
 print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
 
-model.save("model.h5")
+model.save("./Practice/Classified.h5")
 print("Model saved")
-"""
 
-model = load_model("model.h5")
+"""
+#Reloading the model for evaluation and continual training
+model = load_model("./Practice/Classified.h5")
 print("Model Loaded")
 model.summary()
+history = model.fit(xtrain, targets_train, batch_size=128, epochs=100, verbose=1, validation_split=0.3)
 test = model.evaluate(xtest,targets_test,verbose=1)
 print("%s: %.2f%%" % (model.metrics_names[1], test[1]*100))
 
-a = []
-a = np.zeros((2,3,4))
-print(a)
-
-
-mesh = (o3d.io.read_triangle_mesh("cube.stl"))
-vert = np.asarray(mesh.vertices)
-print(vert.ndim)
-
-
-
-model.save("model.h5")
-print("Model saved")
-
-for i in range(1,len(mesh)):
-    history = model.fit(train[i],target[i], epochs=1000, verbose=1)
 model.save(model.h5)
 print("Model saved")
-
-"""
